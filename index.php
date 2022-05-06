36546142684e2ac723425f468ff5e39cf95b477d
Hugo de Oliveira Leal <hugolealtech@gmail.com>
2022-05-06 16:40:21 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Chat IESB</title>
+        <script src="script_head.js"></script>
+        <link rel="StyleSheet" href="estilo.css"/>
+    </head>
+    <body>
+    <script src="script_body.js"></script>
+        <h1>Chat Estágio</h1>
+    </body>
+
+</html>
---
+++ script_head.js
@@ -0,0 +1,1 @@
+alert('Você vai conseguir, Hugão!');
